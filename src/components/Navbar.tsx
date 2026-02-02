64e6007836c71d5b0fbae367bd453ff34c664967
montasirmoyen <montasirmoyen@gmail.com>
2026-02-01 22:44:17 -0500

    Update UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ export default function Navbar() {
     };
 
     return (
-        <nav className="sticky top-0 z-50 bg-navbar shadow-xl shadow-navbar">
+        <nav className="sticky top-0 z-50 backdrop-blur-sm bg-navbar/85 shadow-xl shadow-navbar/75">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-20">
                     <a href="/">
