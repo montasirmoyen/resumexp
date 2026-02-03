77fc8e8b524dfb7c8f08d70094deadd34ffafe86
montasirmoyen <montasirmoyen@gmail.com>
2026-02-03 13:07:35 -0500

    Update rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,24 +5,29 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-/* eslint-disable  @typescript-eslint/no-explicit-any */
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+/* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+/* eslint-disable react/no-unescaped-entities */
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "off"
+    {
+    "rules": {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "prefer-const": "off",
     },
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+  },
+  ignores: [
+    "node_modules/**",
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ],
   },
 ];
 
