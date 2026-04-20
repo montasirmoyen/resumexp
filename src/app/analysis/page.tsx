d5a89c81c33c813d065ae33e9f353e763986ff30
monty <montasirmoyen@gmail.com>
2026-04-20 01:58:24 -0400

    Remove React import for cleaner code in AnalysisPage component

--- src/app/analysis/page.tsx
+++ src/app/analysis/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { AnalysisResult } from '@/types/analysis';
 import { AnalysisDisplay } from '@/components/AnalysisDisplay';
