4988358cdd0766952a794cb95f2ab8676014bb72
monty <montasirmoyen@gmail.com>
2026-04-20 01:46:46 -0400

    Configure Next.js settings for image optimization and development indicators

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    devIndicators: false,
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+}
 
 export default nextConfig
